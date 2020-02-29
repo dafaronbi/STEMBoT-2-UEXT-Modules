761184c6df181e92f4c17e565ffb856b3764425f
Daniel Faronbi <dprdctions@gmail.com>
2020-02-28 20:53:37 -0600

    deleted useless files

--- Software/LPC4088/LPC Workspace/lpc_chip_40xx/Debug/src/ring_buffer.d
+++
@@ -1,7 +0,0 @@
-src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- /Users/danielfaronbi/Google\ Drive/UNO/Fall\ 2019\ Semmester/Embedded\ Microcontrollers/LPC\ Workspace/lpc_chip_40xx/inc/ring_buffer.h \
- /Users/danielfaronbi/Google\ Drive/UNO/Fall\ 2019\ Semmester/Embedded\ Microcontrollers/LPC\ Workspace/lpc_chip_40xx/inc/lpc_types.h
-
-/Users/danielfaronbi/Google\ Drive/UNO/Fall\ 2019\ Semmester/Embedded\ Microcontrollers/LPC\ Workspace/lpc_chip_40xx/inc/ring_buffer.h:
-
-/Users/danielfaronbi/Google\ Drive/UNO/Fall\ 2019\ Semmester/Embedded\ Microcontrollers/LPC\ Workspace/lpc_chip_40xx/inc/lpc_types.h:
--- Software/LPC4088/LPC Workspace/lwip_tcpecho_sa/Debug/example/src/cr_startup_lpc40xx.d
+++
@@ -1,2 +0,0 @@
-example/src/cr_startup_lpc40xx.o example/src/cr_startup_lpc40xx.d: \
- ../example/src/cr_startup_lpc40xx.c
