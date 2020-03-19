13a63948f0c7cf9f655529421c0bb77bd058ee91
Daniel Faronbi <dprdctions@gmail.com>
2020-03-19 16:57:50 -0500

    working code for spi read

--- Software/LPC11C24/adc-spi-test/src/adc-spi-test.c
+++ Software/LPC11C24/adc-spi-test/src/adc-spi-test.c
@@ -4,48 +4,59 @@
  Author      : $(author)
  Version     :
  Copyright   : $(copyright)
- Description : main definition
+ Description : test spi with adc from shift register
 ===============================================================================
 */
 
-#if defined (__USE_LPCOPEN)
-#if defined(NO_BOARD_LIB)
+
 #include "chip.h"
-#else
-#include "board.h"
-#endif
-#endif
+
+
+
 
 #include <cr_section_macros.h>
 
-// TODO: insert other include files here
 
-// TODO: insert other definitions and declarations here
+
+static Chip_SSP_DATA_SETUP_T xf_setup;
 
 int main(void) {
 
-#if defined (__USE_LPCOPEN)
     // Read clock settings and update SystemCoreClock variable
     SystemCoreClockUpdate();
-#if !defined(NO_BOARD_LIB)
-    // Set up and initialize all required blocks and
-    // functions related to the board hardware
-    Board_Init();
-    // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
-#endif
-#endif
 
-    // TODO: insert code here
+    //set up spi
+    Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO0_8, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* MISO0 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO0_9, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* MOSI0 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO0_2, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* SSEL0 */
+	Chip_IOCON_PinMuxSet(LPC_IOCON, IOCON_PIO2_11, (IOCON_FUNC1 | IOCON_MODE_INACT));	/* SCK0 */
+	Chip_IOCON_PinLocSel(LPC_IOCON, IOCON_SCKLOC_PIO2_11);
+    Chip_SSP_Init(LPC_SSP0);
+    Chip_SSP_Enable(LPC_SSP0);
 
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
+    uint8_t val = 0xaa;
+
+    //send output
+    xf_setup.length = 1;
+	xf_setup.tx_data = &val;
+	xf_setup.rx_data = &val;
+	xf_setup.rx_cnt = xf_setup.tx_cnt = 0;
+//    Chip_SSP_RWFrames_Blocking(LPC_SSP0, &xf_setup);
+
+    uint8_t received[8];
+
+//    while(Chip_SSP_GetStatus(LPC_SSP0, SSP_STAT_TNF) == SET){
+//    	Chip_SSP_SendFrame(LPC_SSP0, 0xaa);
+//    }
+
+//    while (Chip_SSP_GetStatus(LPC_SSP0, SSP_STAT_RNE) == SET) {
+    while(1){
+    				received[0] = Chip_SSP_ReceiveFrame(LPC_SSP0);	/* read dummy data */
+    }
+//    			}
+
     while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
+
     }
     return 0 ;
 }
