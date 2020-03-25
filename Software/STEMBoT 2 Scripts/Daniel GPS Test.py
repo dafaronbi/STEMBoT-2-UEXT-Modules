2352516ccf27b52b354e6453ed0fc07cb8d2ccf9
Daniel Faronbi <dprdctions@gmail.com>
2020-03-24 22:00:09 -0500

    add first board program

---
+++ Software/STEMBoT 2 Scripts/Daniel GPS Test.py
@@ -0,0 +1,21 @@
+# writes Daniel's GPS Test on screen
+
+import lcd
+import uasyncio
+
+#make lcd object
+LCD = lcd.screen()
+
+#set backgournd color cyan
+background = LCD.colorRGB(0,255,100)
+
+#set text color
+text = LCD.colorRGB(255,255,255)
+
+
+async def main():
+	#fill background
+	LCD.paint(background)
+	#print text
+	LCD.print("Daniel's GPS Test",40,120,text)
+	while True:
